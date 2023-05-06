124f6aafe6cbf5ab36b61dac269776d53dcb694a
Zhilan Mohammed <88585374+moody-07@users.noreply.github.com>
2023-05-06 21:05:35 +0300

    Initial commit

---
+++ Class2.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+public class TransparentPictureBox : PictureBox
+{
+    public TransparentPictureBox()
+    {
+        SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+        BackColor = Color.Transparent;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace fuck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
